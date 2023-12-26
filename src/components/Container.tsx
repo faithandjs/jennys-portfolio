f6a940180afa92364e8afcfa72e646a867150b90
faithandjs <faiththe1rst@gmail.com>
2023-12-26 15:49:09 +0100

    coming soon banner

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -4,7 +4,10 @@ export default function Container({ children }: { children: React.ReactNode }) {
   return (
     <div className="container">
       <header className="header">
-        <span className="header__title">Jennifer Chinabu</span>
+        <span className="header__title">
+          <span> Jennifer Chinabu</span>
+          <span>Data Scientist</span>
+        </span>
       </header>
       <main>{children}</main>
       {/* <footer className="footer"></footer> */}
