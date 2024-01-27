78f8d96b0364064b4453076e03e7f98f5b3d7ec7
faithandjs <faiththe1rst@gmail.com>
2024-01-27 14:47:21 +0100

    attemp to fix sanity deploy

--- src/lib/sanity.api.ts
+++ src/lib/sanity.api.ts
@@ -5,11 +5,6 @@ export const useCdn = false
  * Importing other npm packages here could lead to needlessly increasing the client bundle size, or end up in a server-only function that don't need it.
  */
 
-export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET',
-)
-
 export const projectId = assertValue(
   process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID',
@@ -24,6 +19,11 @@ export const apiVersion =
 // This is the document id used for the preview secret that's stored in your dataset.
 // The secret protects against unauthorized access to your draft content and have a lifetime of 60 minutes, to protect against bruteforcing.
 export const previewSecretId: `${string}.${string}` = 'preview.secret'
+
+export const dataset = assertValue(
+  process.env.SANITY_STUDIO_DATASET ?? 'production',
+  'Missing environment variable: SANITY_STUDIO_DATASET',
+)
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
   if (v === undefined) {
